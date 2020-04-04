68ed8bf07d0718569918d9c58f5f6b079c061bd2
Togami Yuki <togaminnnn@gmail.com>
2020-04-04 18:13:03 +0900

    Tabバーの追加

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { TimeLineScreen } from "./screens/TimeLineScreen";
+import { AddPostScreen } from "./screens/AddPostScreen";
+import { ProfileScreen } from "./screens/ProfileScreen";
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === "TimeLine") {
+              iconName = "md-home";
+            } else if (route.name === "AddPost") {
+              iconName = "md-paper-plane";
+            } else if (route.name === "Profile") {
+              iconName = "md-person";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "white",
+          inactiveTintColor: "white",
+          activeBackgroundColor: "#483d8b",
+          inactiveBackgroundColor: "#6a5fad",
+        }}
+      >
+        <Tab.Screen name="TimeLine" component={TimeLineScreen} />
+        <Tab.Screen name="AddPost" component={AddPostScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/TimeLineScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export function TimeLineScreen() {
+  return (
+    <View>
+      <Text>TimeLineScreen</Text>
+    </View>
+  );
+}
